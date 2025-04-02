430792cc7b3f26267517e60a8fb927352dbe4ec0
Иван Колганов <110064602+IMKolganov@users.noreply.github.com>
2025-04-02 07:49:09 +0300

    upd git ignore

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,10 +1,19 @@
 const { createProxyMiddleware } = require('http-proxy-middleware');
 
 module.exports = function (app) {
+  const target = process.env.REACT_APP_PROXY_TARGET;
+
+  if (!target) {
+    console.log("[Proxy] ❌ No REACT_APP_PROXY_TARGET — skipping");
+    return;
+  }
+
+  console.log(`[Proxy] ➜ Proxying '/api' to ${target}`);
+
   app.use(
     '/api',
     createProxyMiddleware({
-      target: 'http://localhost:5581',
+      target,
       changeOrigin: true,
       ws: true,
     })
